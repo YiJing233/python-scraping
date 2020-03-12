bb0d6cb2f71daed462ab9e92d3f84b8bf4eb16fa
YiKing <15567055305@163.com>
2020-03-12 19:08:08 +0800

    add some comments

--- v1/chapter14/1-socks.py
+++ v1/chapter14/1-socks.py
@@ -2,6 +2,8 @@ import socks
 import socket
 from urllib.request import urlopen
 
+#socket编程
+
 socks.set_default_proxy(socks.SOCKS5, "127.0.0.1", 9150)
 socket.socket = socks.socksocket
 print(urlopen('http://icanhazip.com').read())
